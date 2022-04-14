2fe65744fec7a0bfc1cbd6a70eac75f23865c991
0xtrevor <zetazeros@protonmail.com>
2022-04-13 21:06:28 -0700

    Proxy 1

---
+++ contracts/Proxy.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >= 0.6.0 < 0.8.0;
+
+import "./Beacon.sol";
+
+
+contract Proxy {
+    // bytes32(uint256(keccak256("eip1967.proxy.beacon")) - 1))
+    bytes32 private constant _BEACON_SLOT = 0xa3f0ad74e5423aebfd80d3ef4346578335a9a72aeaee59ff6cb3582b35133d50;
+
+    // TODO: This proxy could be minimalized, since the beacon address is not intended to change.
+    // Instead of keeping the beacon address in storage, it can be hardcoded into the code of a minimal proxy,
+    // assembled in construction time.
+    constructor() {
+        // TODO: require beacon to be a contract.
+        address beacon = msg.sender;
+
+        // Store beacon address at a custom slot
+        assembly {
+            sstore(_BEACON_SLOT, beacon)
+        }
+    }
+
+}
